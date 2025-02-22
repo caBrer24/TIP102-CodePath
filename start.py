85771f99a96cf7ca945308431c785794062402d9
Carlos Cabrera <carlos.cabrecar@gmail.com>
2025-02-22 12:04:50 -0600

    UPI example approach

--- start.py
+++ start.py
@@ -1,6 +1,17 @@
-print("hello world")
+def nanana_batman(n):
+    """
+    This function prints "nanana" n times.
 
-for i in range(100):
-    print(i)
+ 
+          
+    """
+    result = ""
+
+    for i in range(n):
+        result += "na"
+    return result + " batman!" if result else 'batman!'
+
 
     
+x = nanana_batman(0)
+print(x)
